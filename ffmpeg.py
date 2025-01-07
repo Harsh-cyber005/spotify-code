becf2b680c9741afc479e1ad2d72ef1719f0e9d4
Harsh-cyber005 <harshxgupta931@gmail.com>
2025-01-07 12:01:08 +0000

    linux fix

--- ffmpeg.py
+++ ffmpeg.py
@@ -15,18 +15,23 @@ def get_ffmpeg():
     elif platform_name == "Linux":
         if os.path.exists("./binaries/ffmpeg-linux64-v4.2.2"):
             print("ffmpeg-linux64-v4.2.2 already exists")
-            return "./binaries/ffmpeg-linux64-v4.2.2"
+            full_path = os.getcwd() + "/binaries/ffmpeg-linux64-v4.2.2"
+            os.system(f"chmod +x {full_path}")
+            return full_path
 
     if platform_name == "Windows":
         url = "https://raw.githubusercontent.com/Harsh-cyber005/ffmpeg-win64-v.4.2.2/refs/heads/master/ffmpeg-win64-v4.2.2.exe"
         output = "./binaries/ffmpeg-win64-v4.2.2.exe"
     elif platform_name == "Linux":
         url = "https://raw.githubusercontent.com/Harsh-cyber005/ffmpeg-win64-v.4.2.2/refs/heads/master/ffmpeg-linux64-v4.2.2"
-        output = "./binaries/ffmpeg-linux64-v4.2.2"
+        output = os.getcwd() + "/binaries/ffmpeg-linux64-v4.2.2"
     if not os.path.exists("./binaries"):
         os.makedirs("./binaries")
 
     r = requests.get(url, allow_redirects=True)
     open(output, 'wb').write(r.content)
+
+    if platform_name == "Linux":
+        os.system(f"chmod +x {output}")
 
     return output
